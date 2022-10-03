361f8b0bd40a2183635bb4cc2ec0e45a64140a4a
Atanas Chuchulev <atanas.v4@gmail.com>
2022-10-03 14:51:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "run_some_code" {
   provisioner "local-exec" {
-    command = "echo $TF_WORKSPACE : ${terraform.workspace}"
+    command = "echo ${TF_WORKSPACE} : ${terraform.workspace}"
   }
 }
